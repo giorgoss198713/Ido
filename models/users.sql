a613ea12a4e9a8398e90284c7440f67f2d7b62b4
george.karageorgiou@gmail.com <george.karageorgiou@gmail.com>
2024-04-15 11:17:22 +0000

    set not null columns

--- models/users.sql
+++ models/users.sql
@@ -3,7 +3,11 @@ select * from public.users
 {{ config(
     post_hook=[
       "ALTER TABLE ONLY users ALTER COLUMN id SET DEFAULT nextval('users_id_seq'::regclass);",
+      "ALTER TABLE ONLY users ALTER COLUMN id SET NOT NULL",
       "ALTER TABLE ONLY users ALTER COLUMN password TYPE jsonb USING password::jsonb;",
-      "UPDATE users SET username = md5(username);"
+      "ALTER TABLE ONLY users ALTER COLUMN password SET NOT NULL",
+      "UPDATE users SET username = md5(username);",
+      "ALTER TABLE ONLY users ALTER COLUMN username SET NOT NULL",
+      "ALTER TABLE ONLY users ALTER COLUMN type SET NOT NULL"
     ]
 ) }}
